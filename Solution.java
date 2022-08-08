9c36d364f26c66ead8de336fb2969b103f0b0ac6
Vedant Gotra <88156427+Vedantgotra@users.noreply.github.com>
2022-08-08 21:56:38 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int n=x,num=0,rem;
+       if(n<0)
+           return false;
+      else {
+        while(n!=0){
+            rem=n%10;
+            num=num*10+rem;
+            n=n/10;
+        }
+         return num==x;
+      }
+    }
+}
